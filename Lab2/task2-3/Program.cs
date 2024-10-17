0a19ab8387c0f6de5eb2f86c96c812acd454b718
Kr1p0chek <145468395+Kr1p0chek@users.noreply.github.com>
2024-10-18 01:26:30 +0500

    Add files via upload

---
+++ Lab2/task2-3/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using Lab2_2_3_C__;
+
+internal class Program
+{
+    private static void Main()
+    {
+        bool start = true;
+        while (start)
+        {
+            Console.Clear();
+            try
+            {
+                byte h1, h2, m1, m2;
+                Console.WriteLine("Привет, эта программа для работы с электронными часами, давай установим время на часах");
+                Program program = new Program();
+                Time time1 = program.ins();
+                Console.WriteLine("Установим время на вторых часах");
+                Time time2 = program.ins();
+                Console.Clear();
+                Console.WriteLine($"Первые часы показывают - {time1}");
+                Console.WriteLine($"Вторые часы показывают - {time2}");
+                Console.WriteLine("Увеличим время на первых часах на 1 минуту, а на вторых уменьшим на 1 минуту");
+                time1++;
+                time2--;
+                Console.WriteLine($"Теперь первые часы показывают - {time1}, а вторые - {time2}\n");
+                Console.WriteLine("Ещё раз увеличим время на первых часах на 1 минуту, а на вторых уменьшим на 1 минуту");
+                time1++;
+                time2--;
+                Console.WriteLine($"Теперь первые часы показывают - {time1}, а вторые - {time2}\n");
+                Console.WriteLine($"Сравним, время на часах 1 < времени на часах 2 - {time1 < time2}");
+                Console.WriteLine($"Сравним, время на часах 1 > времени на часах 2 - {time1 > time2}\n");
+                Time time3 = time1.minus(time2);
+                Console.WriteLine("Настроим время на третьих часах, время на них - это время на первых часах минус время на вторых часах");
+                Console.WriteLine($"Третьи часы показывают - {time3}\n");
+                Console.WriteLine("Время на третьих часах в минутах: " + (int)time3);
+                Console.WriteLine("\nКонец, если хочешь запустить программу заного введи 1");
+            }
+            catch (System.OverflowException) { Console.WriteLine("Введённое вами значение не соответствует условию, введите 1, чтобы перезапустить программу и попробуйте снова"); }
+            catch (System.FormatException) { Console.WriteLine("Введённое вами значение не является числом, введите 1, чтобы перезапустить программу и попробуйте снова"); }
+            string restart = Console.ReadLine();
+            if (restart != "1") break;
+        }
+    }
+
+    public Time ins()
+    {
+        string h, m;
+        byte hours, minutes;
+        Console.WriteLine("Введи часы от 0 до 23 (если хочешь установить значения 00:00, то просто нажми Enter):");
+        h = Console.ReadLine();
+        if (h == "")
+            return new Time();
+        else
+        {
+            hours = Convert.ToByte(h);
+            if (hours < 0 || hours > 23)
+                throw new OverflowException();
+        }
+        Console.WriteLine("Введи минуты от 0 до 59 (если хочешь установить 0 минут, то можешь нажать Enter):");
+        m = Console.ReadLine();
+        if (m == "") return new Time(hours, 0);
+        else
+        {
+            minutes = Convert.ToByte(m);
+            if (minutes < 0 || minutes > 59) throw new OverflowException();
+            else return new Time(hours, minutes);
+        }
+    }
+}
---
+++ Lab2/task2-3/Time.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2_3_C__
+{
+    internal class Time
+    {
+        private byte hours, minutes;
+
+        public byte HOURS { get { return hours; } set { hours = value; } }
+        public byte MINUTES { get { return minutes; } set { minutes = value; } }
+
+        public Time()
+        {
+            this.hours = 0;
+            this.minutes = 0;
+        }
+
+        public Time(byte hours, byte minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public Time minus(Time time2)
+        {
+            byte minutes = 0, hours = 0;
+            if (this.minutes < time2.minutes && this.hours > 0)
+            {
+                minutes = 60;
+                minutes += this.minutes;
+                minutes -= time2.minutes;
+                hours = this.hours;
+                hours--;
+            }
+            else if (this.minutes < time2.minutes && this.hours == 0)
+            {
+                hours = 23;
+                minutes = 60;
+                minutes += this.minutes;
+                minutes -= time2.minutes;
+            }
+            else
+            {
+                minutes = this.minutes;
+                minutes -= time2.minutes;
+                hours = this.hours;
+            }
+            if (hours >= time2.hours)
+            {
+                hours -= time2.hours;
+            }
+            else
+            {
+                hours += 24;
+                hours -= time2.hours;
+            }
+            return new Time(hours, minutes);
+        }
+        public override string ToString() //перегрузка оператора
+        {
+            string hours, minutes;
+            if (this.hours < 10)
+            {
+                hours = "0" + this.hours;
+            }
+            else
+                hours = Convert.ToString(this.hours);
+            if (this.minutes < 10)
+            {
+                minutes = "0" + this.minutes;
+            }
+            else
+                minutes = Convert.ToString(this.minutes);
+            return hours + ":" + minutes;
+        }
+
+        public static Time operator ++(Time time)
+        {
+            if (time.minutes == 59)
+            {
+                time.minutes = 0;
+                if (time.hours == 23)
+                    time.hours = 0;
+                else
+                    time.hours++;
+            }
+            else
+                time.minutes++;
+            return time;
+        }
+        public static Time operator --(Time time)
+        {
+            if (time.minutes == 0)
+            {
+                if (time.hours > 0)
+                    time.hours--;
+                else
+                    time.hours = 23;
+                time.minutes = 59;
+            }
+            else
+                time.minutes--;
+            return time;
+        }
+
+        public static implicit operator int(Time time)
+        {
+            return time.minutes + time.hours * 60;
+        }
+
+        public static explicit operator bool(Time time)
+        {
+            if (time.hours == 0 && time.minutes == 0)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator >(Time time1, Time time2)
+        {
+            if (0 + time1 > 0 + time2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Time time1, Time time2)
+        {
+            if (0 + time1 < 0 + time2)
+                return true;
+            else
+                return false;
+        }
+    }
+}
